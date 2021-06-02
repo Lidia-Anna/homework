246dc348e0b8cf6969506c0030ccdcf73d1909b5
Lidiia <Lidiia@MADEBOOK>
2021-06-02 11:59:20 +0200

    my first commit

---
+++ HomeWork/HomeWork2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static string checkMyLine()
+        {
+
+
+            int myline = 0;
+            string line;
+
+            line = Console.ReadLine();
+            myline = int.Parse(line);
+            if ((myline != 1) & (myline != 2) & (myline != 3))
+            {
+                Console.WriteLine("Please follow the instructions and try again");
+                return checkMyLine();
+            }
+
+            return line;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("About which language do you want to know information? 1 - English, 2 - Spanish, 3 - Ukrainian ");
+            string line = checkMyLine();
+            byte languageValue;
+            languageValue = byte.Parse(line);
+            Console.WriteLine($"With 2,500 to 3,000 words, you can understand 90% of everyday {(Language)languageValue} conversations,");
+            Console.WriteLine($"{(Language)languageValue} newspaper and magazine articles, and {(Language)languageValue} used in the workplace.");
+            Console.WriteLine($"So it is essential to learn the right {(Language)languageValue} vocabulary words.");
+        }
+        public enum Language : byte
+        {
+            English = 1,
+            Spanish = 2,
+            Ukrainian = 3
+        }
+    }
+}
+
---
+++ Lecture21Demo/Lecture21Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lecture21Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
